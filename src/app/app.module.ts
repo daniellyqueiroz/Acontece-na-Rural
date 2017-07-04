a937f6a71e481f84ec3ebbe96cdfaf6b3e6729ca
daniellyqueiroz <danielly.queiroz@outlook.com>
2017-07-04 18:57:13 -0300

    implementacao Mural

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule }   from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { TimelineComponent } from './timeline/timeline.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TimelineComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      {
+	    path: '',
+	    component: TimelineComponent
+	  },
+	  {
+	    path: 'timeline',
+	    component: TimelineComponent
+	  }
+	])
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+
+
+
