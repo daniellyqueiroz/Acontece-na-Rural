e03ac7961d1a4db2715ed326e8c9a970641049cf
daniellyqueiroz <danielly.queiroz@outlook.com>
2017-07-16 23:32:06 -0300

    Criação dos comentários e validações dos campos: publicação e comentário.

--- src/app/admin/feed-noticias/feed-noticias.component.ts
+++ src/app/admin/feed-noticias/feed-noticias.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormsModule }   from '@angular/forms'; 
 
 @Component({
   selector: 'app-feed-noticias',
@@ -10,6 +11,8 @@ export class FeedNoticiasComponent implements OnInit {
 
   public feed: any[];
   public usuarioLogado: any;
+  public textValue: string = '';
+  
 
   constructor() { 
 
@@ -33,68 +36,151 @@ export class FeedNoticiasComponent implements OnInit {
   			data: 'Publicado - 7:30 PM Hoje',
   			qntCurtidas: '98',
   			qntComentarios: '2',
-  			comentarios: [
-  				
-  			]
+  			comentarios: [{
+  				id :11,
+          idPublicacao: 54,
+          idUsuario: 8,
+          nome: 'Joao',
+          curso: 'BCC',
+          imagem: "assets/img/joao.jpg",
+          texto: "meu comentario",
+          data: '15/07/2017 22:58:14',
+          qntCurtidas: 3
+          },
+          {
+          id :11,
+          idPublicacao: 54,
+          idUsuario: 8,
+          nome: 'Joao',
+          curso: 'BCC',
+          imagem: "assets/img/joao.jpg",
+          texto: "meu comentario",
+          data: '15/07/2017 22:58:14',
+          qntCurtidas: 3
+          },
+          {
+          id :11,
+          idPublicacao: 54,
+          idUsuario: 8,
+          nome: 'Joao',
+          curso: 'BCC',
+          imagem: "assets/img/joao.jpg",
+          texto: "meu comentario",
+          data: '15/07/2017 22:58:14',
+          qntCurtidas: 3
+          }]
 
   		  },
           {
-        id: '11',
-        nome: this.usuarioLogado.nome,
-        imagem: this.usuarioLogado.imagem,
-        texto : 'teste',
-        fotos: [],
-        videos: [],
-        data: 'Publicado - 7:30 PM Hoje',
-        qntCurtidas: '98',
-        qntComentarios: '2',
-        comentarios: [
-         
-        ]
-
-        },
+            id: '11',
+            nome: this.usuarioLogado.nome,
+            imagem: this.usuarioLogado.imagem,
+            texto : 'teste',
+            fotos: [],
+            videos: [],
+            data: 'Publicado - 7:30 PM Hoje',
+            qntCurtidas: '98',
+            qntComentarios: '2',
+            comentarios: [{
+              id :32,
+              idPublicacao: 65,
+              idUsuario: 76,
+              nome: 'Joao',
+              curso: 'BCC',
+              imagem: "assets/img/fotouser.jpg",
+              texto: "meu comentario",
+              data: '15/07/2017 22:58:14',
+              qntCurtidas: 88
+              }
+            ]
+          },
           {
-        id: '6',
-        nome: this.usuarioLogado.nome,
-        imagem: this.usuarioLogado.imagem,
-        texto : 'teste',
-        fotos: [],
-        videos: [],
-        data: 'Publicado - 7:30 PM Hoje',
-        qntCurtidas: '98',
-        qntComentarios: '2',
-        comentarios: [
-          
-        ]
-
-        }
+            id: '6',
+            nome: this.usuarioLogado.nome,
+            imagem: this.usuarioLogado.imagem,
+            texto : 'teste',
+            fotos: [],
+            videos: [],
+            data: 'Publicado - 7:30 PM Hoje',
+            qntCurtidas: '98',
+            qntComentarios: '2',
+            comentarios: [{
+              id:11,
+              idPublicacao: 54,
+              idUsuario: 8,
+              nome: 'Joao',
+              curso: 'BCC',
+              imagem: "assets/img/fotouser.jpg",
+              texto: "meu comentario",
+              data: '15/07/2017 22:58:14',
+              qntCurtidas: 3
+            }]
+          }
   		];
 
-}
+  }
 
 
   ngOnInit() {
   }
 
-    publicacao(pubTexto: any){
+  publicacao(pubTexto: any){
+
     let novaPublicacao= {
-        id: 4,
-        texto: pubTexto,
-        data: "16/07/2017 00:18",
-        fotos: [],
-        videos: [],
-        comentarios: [
-          ],
-        curtidas: 0,
+        
+      id: 4,
+      texto: pubTexto,
+      data: "16/07/2017 00:18",
+      fotos: [],
+      videos: [],
+      comentarios:[],
+      curtidas: 0,
+        
+      nome: this.usuarioLogado.nome,
+      curso: this.usuarioLogado.curso,
+      imagem: this.usuarioLogado.imagem,
+      idUsuario: this.usuarioLogado.id
        
-        nome: this.usuarioLogado.nome,
-        curso: this.usuarioLogado.curso,
-        imagem: this.usuarioLogado.imagem,
-        idUsuario: this.usuarioLogado.id
     }
     
-    this.feed.unshift(novaPublicacao);
+    if(pubTexto!=null && pubTexto.length >0){
+      
+       this.feed.unshift(novaPublicacao);
+    }
+        
   }
+
+  addComentario(publicacao: any, comentarioTexto: any){
+   
+    let novoComentario = {
+      id: 1,
+      idPublicacao:10,
+      texto: comentarioTexto,
+      data: "16/07/2017 18:28",
+      curtidas: 0,
+    
+          nome: publicacao.nome,
+          curso: publicacao.curso,
+          imagem: publicacao.imagem
+      
+    }
+     if(comentarioTexto!=null && comentarioTexto.length >0){
+        publicacao.comentarios.push(novoComentario);
+    }
+
+
+  }
+
+  contCurtidas(){
+    let novaCurtida ={
+      id: 50,
+      idComentario:20,
+      idUsuario: 9,
+      data: "16/07/2017 18:28"
+    }
+  }
+
+
 
 
 }
